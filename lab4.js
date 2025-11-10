80f27d32c18bb9a3480597f896fd486645c69681
Kien916 <kientrungkien133@gmail.com>
2025-11-10 15:09:09 +0700

    lab4

--- lab4.js
+++ lab4.js
@@ -1,6 +1,6 @@
 function getFirstLast(array) {
   // Your code here
-  const [,...number] = getFirstLast
+  const [first,...number,last] = getFirstLast
 }
 
 console.log(getFirstLast([1, 2, 3, 4])); // [1, 4]
